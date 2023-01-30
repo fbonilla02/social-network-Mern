a34a09b2c5c95a9a35c9708ad34680ba357d2ad0
fbonilla <franciscobonillac@gmail.com>
2023-01-30 15:06:20 -0500

    cambio para los archivos estaticos

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv"
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import path from "path"
+import path, {join} from "path"
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
@@ -23,6 +23,7 @@ dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+console.log(__dirname);
 const app = express()
 app.use(express.json())
 app.use(helmet())
@@ -53,7 +54,7 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost)
 app.use("/auth", authRoutes)
 app.use("/users", userRoutes)
 app.use("/posts", postRoutes)
-
+app.use(express.static(join(__dirname, '../client/dist')))
 
 /*MONGOOSSE SETUP*/
 
